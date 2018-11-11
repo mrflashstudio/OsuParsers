d4b3417e19d7427ba8d3bedfddd755784b40d7b0
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2018-11-11 11:00:56 -0500

    Make classes static to bypass creating an instance of parser class in code.

--- OsuBeatmapParser/Parser.cs
+++ OsuBeatmapParser/Parser.cs
@@ -8,50 +8,50 @@ namespace OsuBeatmapParser
 {
     public class Parser
     {
-        private StoryboardDecoder storyboardDecoder = new StoryboardDecoder();
-        private BeatmapDecoder beatmapDecoder = new BeatmapDecoder();
-        private DatabaseDecoder databaseDecoder = new DatabaseDecoder();
+        private static StoryboardDecoder storyboardDecoder = new StoryboardDecoder();
+        private static BeatmapDecoder beatmapDecoder = new BeatmapDecoder();
+        private static DatabaseDecoder databaseDecoder = new DatabaseDecoder();
 
         /// <summary>
         /// Parses .osu file.
         /// </summary>
         /// <param name="pathToBeatmap">Path to the .osu file.</param>
         /// <returns>A usable beatmap.</returns>
-        public Beatmap ParseBeatmap(string pathToBeatmap) => beatmapDecoder.Decode(File.ReadAllLines(pathToBeatmap));
+        public static Beatmap ParseBeatmap(string pathToBeatmap) => beatmapDecoder.Decode(File.ReadAllLines(pathToBeatmap));
 
         /// <summary>
         /// Parses .osb file.
         /// </summary>
         /// <param name="pathToStoryboard">Path to the .osb file.</param>
         /// <returns>A usable storyboard.</returns>
-        public Storyboard ParseStoryboard(string pathToStoryboard) => storyboardDecoder.Decode(File.ReadAllLines(pathToStoryboard));
+        public static Storyboard ParseStoryboard(string pathToStoryboard) => storyboardDecoder.Decode(File.ReadAllLines(pathToStoryboard));
 
         /// <summary>
         /// Parses osu!.db file.
         /// </summary>
         /// <param name="pathToOsuDb">Path to the osu!.db file.</param>
         /// <returns>A usable <see cref="OsuDatabase"/>.</returns>
-        public OsuDatabase ParseOsuDatabase(string pathToOsuDb) => databaseDecoder.DecodeOsu(File.OpenRead(pathToOsuDb));
+        public static OsuDatabase ParseOsuDatabase(string pathToOsuDb) => databaseDecoder.DecodeOsu(File.OpenRead(pathToOsuDb));
 
         /// <summary>
         /// Parses collection.db file.
         /// </summary>
         /// <param name="pathToCollectionDb">Path to the collection.db file.</param>
         /// <returns>A usable <see cref="CollectionDatabase"/>.</returns>
-        public CollectionDatabase ParseCollectionDatabase(string pathToCollectionDb) => databaseDecoder.DecodeCollection(File.OpenRead(pathToCollectionDb));
+        public static CollectionDatabase ParseCollectionDatabase(string pathToCollectionDb) => databaseDecoder.DecodeCollection(File.OpenRead(pathToCollectionDb));
 
         /// <summary>
         /// Parses scores.db file.
         /// </summary>
         /// <param name="pathToScoresDb">Path to the scores.db file.</param>
         /// <returns>A usable <see cref="ScoresDatabase"/>.</returns>
-        public ScoresDatabase ParseScoresDatabase(string pathToScoresDb) => databaseDecoder.DecodeScores(File.OpenRead(pathToScoresDb));
+        public static ScoresDatabase ParseScoresDatabase(string pathToScoresDb) => databaseDecoder.DecodeScores(File.OpenRead(pathToScoresDb));
 
         /// <summary>
         /// Parses presence.db file.
         /// </summary>
         /// <param name="pathToPresenceDb">Path to the presence.db file.</param>
         /// <returns>A usable <see cref="PresenceDatabase"/>.</returns>
-        public PresenceDatabase ParsePresenceDatabase(string pathToPresenceDb) => databaseDecoder.DecodePresence(File.OpenRead(pathToPresenceDb));
+        public static PresenceDatabase ParsePresenceDatabase(string pathToPresenceDb) => databaseDecoder.DecodePresence(File.OpenRead(pathToPresenceDb));
     }
 }
