370fbd069b2ffabc3de342cc3344b2395297a5eb
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2018-12-09 15:12:22 -0500

    Basic standard beatmap writing.

--- OsuParsers/Helpers/FormatHelper.cs
+++ OsuParsers/Helpers/FormatHelper.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OsuParsers.Beatmaps.Objects;
+using OsuParsers.Beatmaps.Objects.Standard;
+using OsuParsers.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +13,118 @@ namespace OsuParsers.Helpers
     {
         public static string Join(IEnumerable<string> vs, string splitter = " ")
         {
-            string owo = string.Empty;
-            vs.ToList().ForEach(e => owo += e + splitter);
-            return owo;
+            if (vs != null)
+            {
+                string owo = string.Empty;
+                vs.ToList().ForEach(e => owo += e + splitter);
+                return owo.TrimEnd();
+            }
+            else
+                return string.Empty;
         }
 
         public static string Join(IEnumerable<int> vs, string splitter = " ")
         {
-            List<string> x = vs.ToList().ConvertAll(e => e.ToString());
-            return Join(x, splitter);
+            if (vs != null)
+            {
+                List<string> x = vs.ToList().ConvertAll(e => e.ToString());
+                return Join(x, splitter);
+            }
+            else
+                return string.Empty;
+        }
+
+        public static int Bool(bool value) => value ? 1 : 0;
+
+        public static string TimingPoint(TimingPoint timingPoint)
+        {
+            var offset = timingPoint.Offset;
+            var msPerBeat = timingPoint.BeatLength;
+            var meter = (int)timingPoint.TimeSignature;
+            var sampleSet = (int)timingPoint.SampleSet;
+            var sampleIndex = timingPoint.CustomSampleSet;
+            var volume = timingPoint.Volume;
+            var inherited = Bool(timingPoint.Inherited);
+            var kiaiMode = Bool(timingPoint.KiaiMode);
+
+            return $"{offset},{msPerBeat},{meter},{sampleSet},{sampleIndex},{volume},{inherited},{kiaiMode}";
+        }
+
+        public static string HitObject(HitObject hitObject)
+        {
+            var x = hitObject.Position.X;
+            var y = hitObject.Position.Y;
+            var time = hitObject.StartTime;
+            var hitsound = (int)hitObject.HitSound;
+            var extras = HitObjectExtras(hitObject.Extras);
+            int type;
+            
+            //TODO: add proper type implementation
+            //TODO: add all object types
+
+            if (hitObject is StandardHitCircle standardHitCircle)
+            {
+                type = (Bool(standardHitCircle.IsNewCombo) * 4) + 1;
+                return $"{x},{y},{time},{type},{hitsound},{extras}";
+            }
+            if (hitObject is StandardSlider standardSlider)
+            {
+                type = (Bool(standardSlider.IsNewCombo) * 4) + 2;
+                return $"{x},{y},{time},{type},{hitsound},{SliderProperties(standardSlider)},{extras}";
+            }
+            if (hitObject is StandardSpinner standardSpinner)
+            {
+                type = (Bool(standardSpinner.IsNewCombo) * 4) + 4;
+                return $"{x},{y},{time},{type},{hitsound},{standardSpinner.EndTime},{extras}";
+            }
+            throw new NotImplementedException();
+        }
+
+        public static string SliderProperties(StandardSlider slider)
+        {
+            var sliderType = CurveType(slider.CurveType);
+
+            string sliderPoints = string.Empty;
+            slider.SliderPoints.ForEach(pt => sliderPoints += $"|{pt.X}:{pt.Y}");
+
+            var repeats = slider.Repeats;
+            var pixelLength = slider.PixelLength;
+
+            string edgeHitsounds = string.Empty;
+            slider.EdgeHitSounds.ForEach(sound => edgeHitsounds += $"{(int)sound}|");
+
+            string edgeAdditions = string.Empty;
+            slider.EdgeAdditions.ToList().ForEach(e => edgeAdditions += $"{(int)e.Item1}:{(int)e.Item2}");
+
+            return $"{sliderType},{sliderPoints},{repeats},{pixelLength},{edgeHitsounds},{edgeAdditions}";
+        }
+
+        public static char CurveType(CurveType value)
+        {
+            switch (value)
+            {
+                case Enums.CurveType.Bezier:
+                    return 'B';
+                case Enums.CurveType.Catmull:
+                    return 'C';
+                case Enums.CurveType.Linear:
+                    return 'L';
+                case Enums.CurveType.PerfectCurve:
+                    return 'P';
+                default:
+                    throw new InvalidCastException();
+            }
+        }
+
+
+        public static string HitObjectExtras(HitObjectExtras extras)
+        {
+            var sampleSet = (int)extras.SampleSet;
+            var additionSet = (int)extras.AdditionSet;
+            var customIndex = extras.CustomIndex;
+            var sampleVolume = extras.Volume;
+            var filename = extras.SampleFileName ?? string.Empty;
+            return $"{sampleSet}:{additionSet}:{customIndex}:{sampleVolume}:{filename}";
         }
     }
 }
---
+++ OsuParsers/Writers/BeatmapWriter.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OsuParsers.Beatmaps;
+using OsuParsers.Beatmaps.Sections;
+using OsuParsers.Beatmaps.Objects;
+
+namespace OsuParsers.Writers
+{
+    public class BeatmapWriter
+    {
+        public static List<string> Write(Beatmap beatmap)
+        {
+            var Sections = new List<List<string>>
+            {
+                GeneralSection(beatmap.GeneralSection),
+                EditorSection(beatmap.EditorSection),
+                MetadataSection(beatmap.MetadataSection),
+                EventsSection(beatmap.EventsSection),
+                TimingPoints(beatmap.TimingPoints),
+                HitObjects(beatmap.HitObjects),
+            };
+
+            List<string> contents = new List<string>
+            {
+                $"osu file format v{beatmap.Version}",
+            };
+            Sections.ForEach(stringList => stringList.ForEach(item => contents.Add(item)));
+            return contents;
+        }
+
+        private static NumberFormatInfo NumFormat = new CultureInfo(@"en-US", false).NumberFormat;
+
+        #region Sections
+
+        public static List<string> GeneralSection(GeneralSection section)
+        {
+            return new List<string>
+            {
+                string.Empty,
+                "[General]",
+                "AudioFilename: " + section.AudioFilename,
+                "AudioLeadIn: " + section.AudioLeadIn,
+                "PreviewTime: " + section.PreviewTime,
+                "Countdown: " + Helpers.FormatHelper.Bool(section.Countdown),
+                "SampleSet: " + section.SampleSet,
+                "StackLeniency: " + section.StackLeniency.ToString(NumFormat),
+                "Mode: " + (int)section.Mode,
+                "LetterboxInBreaks: " + Helpers.FormatHelper.Bool(section.LetterboxInBreaks),
+                "WidescreenStoryboard: " + Helpers.FormatHelper.Bool(section.WidescreenStoryboard),
+                "StoryFireInFront: " + Helpers.FormatHelper.Bool(section.StoryFireInFront),
+                "SpecialStyle: " + Helpers.FormatHelper.Bool(section.SpecialStyle),
+                "EpilepsyWarning: " + Helpers.FormatHelper.Bool(section.EpilepsyWarning),
+                "UseSkinSprites: " + Helpers.FormatHelper.Bool(section.UseSkinSprites),
+            };
+        }
+
+        public static List<string> EditorSection(EditorSection section)
+        {
+            return new List<string>
+            {
+                string.Empty,
+                "[Editor]",
+                "Bookmarks: " + section.BookmarksString,
+                "DistanceSpacing: " + section.DistanceSpacing.ToString(NumFormat),
+                "BeatDivisor: " + section.BeatDivisor.ToString(NumFormat),
+                "GridSize: " + section.GridSize.ToString(NumFormat),
+                "TimelineZoom: " + section.TimelineZoom.ToString(NumFormat),
+            };
+        }
+
+        public static List<string> MetadataSection(MetadataSection section)
+        {
+            return new List<string>
+            {
+                string.Empty,
+                "[Metadata]",
+                "Title:" + section.Title,
+                "TitleUnicode:" + section.TitleUnicode,
+                "Artist:" + section.Artist,
+                "ArtistUnicode:" + section.ArtistUnicode,
+                "Creator:" + section.Creator,
+                "Version:" + section.Version,
+                "Source:" + section.Source,
+                "Tags:" + section.TagsString,
+                "BeatmapID:" + section.BeatmapID,
+                "BeatmapSetID:" + section.BeatmapSetID,
+            };
+        }
+
+        public static List<string> EventsSection(EventsSection section)
+        {
+            return new List<string>
+            {
+                string.Empty,
+                "[Events]",
+                @"//Background and Video events",
+                $"0,0,\"{section.BackgroundImage}\",0,0",
+                @"//Break Periods",
+                //TODO: add break periods
+                //TODO: add storyboard layers
+                @"//Storyboard Layer 0 (Background)",
+                @"//Storyboard Layer 1 (Fail)",
+                @"//Storyboard Layer 2 (Pass)",
+                @"//Storyboard Layer 3 (Foreground)",
+                @"//Storyboard Sound Samples",
+            };
+        }
+
+        public static List<string> TimingPoints(List<TimingPoint> timingPoints)
+        {
+            var list = BaseListFormat("TimingPoints");
+            if (timingPoints != null)
+                list.AddRange(timingPoints.ConvertAll(point => Helpers.FormatHelper.TimingPoint(point)));
+            return list;
+        }
+
+        public static List<string> HitObjects(List<HitObject> hitObjects)
+        {
+            var list = BaseListFormat("HitObjects");
+            if (hitObjects != null)
+                list.AddRange(hitObjects.ConvertAll(obj => Helpers.FormatHelper.HitObject(obj)));
+            return list;
+        }
+
+        #endregion
+
+        public static List<string> BaseListFormat(string SectionName)
+        {
+            return new List<string>
+            {
+                string.Empty,
+                $"[{SectionName}]",
+            };
+        }
+    }
+}
