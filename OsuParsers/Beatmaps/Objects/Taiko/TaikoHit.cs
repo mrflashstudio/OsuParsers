9004e1f2906e332a465c39993b222707e5f64cff
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2018-12-31 17:50:06 -0500

    Add getter for "IsBig" property of TaikoHit
    TODO: add setter

--- OsuParsers/Beatmaps/Objects/Taiko/TaikoHit.cs
+++ OsuParsers/Beatmaps/Objects/Taiko/TaikoHit.cs
@@ -5,13 +5,19 @@ namespace OsuParsers.Beatmaps.Objects.Taiko
 {
     public class TaikoHit : Circle
     {
-        public bool IsBig { get; set; }
+        public bool IsBig
+        {
+            get
+            {
+                return HitSound.HasFlag(HitSoundType.Finish);
+            }
+        }
         public TaikoColor Color { get; set; }
 
         public TaikoHit(Point position, int startTime, int endTime, HitSoundType hitSound, Extras extras, bool isBig, TaikoColor color) 
             : base(position, startTime, endTime, hitSound, extras, false, 0)
         {
-            IsBig = isBig;
+            //IsBig = isBig; (Needs to be finished, commented out to keep some functionality)
             Color = color;
         }
     }
