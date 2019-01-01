1b3e420c1992dd982512b7f2e1795733928b0459
InfinityGhost <39218853+InfinityGhost@users.noreply.github.com>
2019-01-01 13:12:04 -0500

    Implement setter & getter for IsBig and Color

--- OsuParsers/Beatmaps/Objects/Taiko/TaikoHit.cs
+++ OsuParsers/Beatmaps/Objects/Taiko/TaikoHit.cs
@@ -11,13 +11,38 @@ namespace OsuParsers.Beatmaps.Objects.Taiko
             {
                 return HitSound.HasFlag(HitSoundType.Finish);
             }
+            set
+            {
+                if (value)
+                    HitSound += (int)HitSoundType.Finish;
+                else
+                    if (HitSound.HasFlag(HitSoundType.Finish))
+                        HitSound -= (int)HitSoundType.Finish;
+            }
         }
-        public TaikoColor Color { get; set; }
+        public TaikoColor Color
+        {
+            get
+            {
+                if (HitSound.HasFlag(HitSoundType.Whistle) || HitSound.HasFlag(HitSoundType.Clap))
+                    return TaikoColor.Blue;
+                else
+                    return TaikoColor.Red;
+            }
+            set
+            {
+                if (value == TaikoColor.Red)
+                    HitSound = HitSoundType.Normal;
+                else if (value == TaikoColor.Blue)
+                    HitSound = HitSoundType.Whistle;
+            }
+        }
 
         public TaikoHit(Point position, int startTime, int endTime, HitSoundType hitSound, Extras extras, bool isBig, TaikoColor color) 
             : base(position, startTime, endTime, hitSound, extras, false, 0)
         {
-            //IsBig = isBig; (Needs to be finished, commented out to keep some functionality)
+            // These may be unnecessary now
+            IsBig = isBig;
             Color = color;
         }
     }
