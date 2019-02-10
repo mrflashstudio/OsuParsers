f007fef2173d43cc7046e3436ea649ce4e6ed639
andy840119 <andy840119@gmail.com>
2019-02-10 23:47:36 +0900

    add test project

---
+++ OsuParsers.Test/AddTaikoHitObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OsuParsers.Test
+{
+    [TestClass]
+    public class AddTaikoHitObject
+    {
+    }
+}
---
+++ OsuParsers.Test/TestBeatmap.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OsuParsers.Test
+{
+    [TestClass]
+    public class TestBeatmap
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
