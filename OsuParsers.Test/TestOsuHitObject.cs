8d8a3a2b34b4ad6e0e7c79440c8bba6cbd57811a
andy840119 <andy840119@gmail.com>
2019-02-12 01:50:12 +0900

    \All test passed/ TODO : adjust test

--- OsuParsers.Test/TestOsuHitObject.cs
+++ OsuParsers.Test/TestOsuHitObject.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OsuParsers.Beatmaps.Objects;
+using OsuParsers.Enums;
 
 namespace OsuParsers.Test
 {
     [TestClass]
     public class TestOsuHitObject : BaseHitObjectTest
     {
+        [TestMethod]
+        public void TestOsuHitobjects()
+        {
+            var beatmap = GenerateBeatmap(Ruleset.Standard);
+
+            //Create Slider
+            beatmap.HitObjects.Add(new Slider(new Point(100, 100), 1000, 2000, HitSoundType.Clap
+                , CurveType.Bezier, new List<Point>(), 1, 0, new List<HitSoundType>()
+                , new List<Tuple<SampleSet, SampleSet>>(), new Extras(), false, 0));
+
+            //Create HitObject
+            beatmap.HitObjects.Add(new Circle(new Point(200, 200), 3000, 4000, HitSoundType.Clap, new Extras(), true, 0));
+
+            //Create Spinner
+            beatmap.HitObjects.Add(new Spinner(new Point(300, 300), 5000, 6000, HitSoundType.Finish, new Extras(), false, 0));
+
+            //compare with exported beatmap
+            Assert.IsTrue(CompareOriginAndReloadBeatmap(beatmap));
+        }
     }
 }
--- OsuParsers.Test/TestTaikoHitObject.cs
+++ OsuParsers.Test/TestTaikoHitObject.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OsuParsers.Beatmaps.Objects;
+using OsuParsers.Beatmaps.Objects.Taiko;
+using OsuParsers.Enums;
 
 namespace OsuParsers.Test
 {
     [TestClass]
     public class TestTaikoHitObject : BaseHitObjectTest
     {
+        [TestMethod]
+        public void TestTaikoHitobjects()
+        {
+            var beatmap = GenerateBeatmap(Ruleset.Taiko);
+
+            //Create TaikoDrumroll
+            beatmap.HitObjects.Add(new TaikoDrumroll(new Point(100, 100), 1000, 2000, HitSoundType.Clap
+                , CurveType.Bezier, new List<Point>(), 1, 0, new List<HitSoundType>()
+                , new List<Tuple<SampleSet, SampleSet>>(), new Extras(), false, 0));
+
+            //Create TaikoHit
+            beatmap.HitObjects.Add(new TaikoHit(new Point(200, 200), 3000, 4000, HitSoundType.Clap, new Extras(), true, 0));
+
+            //Create TaikoSpinner
+            beatmap.HitObjects.Add(new TaikoSpinner(new Point(300, 300), 5000, 6000, HitSoundType.Finish, new Extras(), false, 0));
+
+            //compare with exported beatmap
+            Assert.IsTrue(CompareOriginAndReloadBeatmap(beatmap));
+        }
     }
 }
